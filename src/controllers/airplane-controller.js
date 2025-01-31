9eaa46e4635856d63da08936813905b385a34d5a
Jignesh <jigneshmal.com>
2025-01-31 10:00:39 +0530

    orm for  mysql intgrated

---
+++ src/controllers/airplane-controller.js
@@ -0,0 +1,29 @@
+const { AirplaneService } = require("../services");
+const { StatusCodes } = require("http-status-codes");
+
+async function createAirplane(req, res) {
+  try {
+    const airplane = await AirplaneService.createAirplane({
+      modelNumber: req.body.modelNumber,
+      capacity: req.body.capacity,
+    });
+
+    return res.status(StatusCodes.CREATED).json({
+      success: true,
+      message: "Successfully created an airplane",
+      data: airplane,
+      error: {},
+    });
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: "Something went wrong while creating airplane",
+      data: {},
+      error: error,
+    });
+  }
+}
+
+module.exports = {
+  createAirplane,
+};
--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ const { ServerConfig } = require("./config");
 const apiRoutes = require("./routes/index");
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
   console.log("Successfully started server on port " + ServerConfig.PORT);
+  // Logger.info("Successfully started the server", {});
 });
